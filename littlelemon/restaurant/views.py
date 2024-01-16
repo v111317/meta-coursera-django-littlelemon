b5943d015895f650bff4337d337101f3cd49bcc0
v111317 <147157983+v111317@users.noreply.github.com>
2024-01-16 20:43:28 +0530

    little - added djoser auth on views

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -4,6 +4,9 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, De
 from rest_framework.viewsets import ModelViewSet
 from .serializers import MenuItemSerializer, BookingSerializer
 from .models import Booking, Menu
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 def say_hello(request):
     return HttpResponse("hello world")
@@ -12,15 +15,25 @@ def index(request):
     return render(request, "index.html", {})
 
 class MenuItemsView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
     
+    
 
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     #lookup_url_kwarg = 'pk'
     serializer_class = MenuItemSerializer    
     
 class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
