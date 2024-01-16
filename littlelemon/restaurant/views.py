de81ad34affd735a649058ea9f3a344f2c809957
v111317 <147157983+v111317@users.noreply.github.com>
2024-01-16 19:35:31 +0530

    little - added booking api via router

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .serializers import MenuItemSerializer, BookingSerializer
+from .models import Booking, Menu
 
 def say_hello(request):
     return HttpResponse("hello world")
 
 def index(request):
     return render(request, "index.html", {})
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+    
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    #lookup_url_kwarg = 'pk'
+    serializer_class = MenuItemSerializer    
+    
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
